0c0cbd0fe674730d430215669623ad08a77cda4e
Igor Shults <shults+github@uwalumni.com>
2014-04-16 09:38:31 -0500

    Swapping developers for author

--- LogHibernateStatsGrailsPlugin.groovy
+++ LogHibernateStatsGrailsPlugin.groovy
@@ -2,10 +2,11 @@ class LogHibernateStatsGrailsPlugin {
     def version = "0.9"
     def grailsVersion = "2.0 > *"
     def title = "Log Hibernate Stats Plugin"
+    def author = "Igor Shults"
+    def authorEmail = "igor.shults@gmail.com"
     def description = 'Logs Hibernate statistics across controller actions'
     def documentation = "https://github.com/ishults/logHibernateStats"
     def license = "APACHE"
-    def developers = [ [ name: "Igor Shults", email: "igor.shults@gmail.com" ]]
     def issueManagement = [ system: "GitHub", url: "https://github.com/ishults/logHibernateStats/issues" ]
     def scm = [ url: "https://github.com/ishults/logHibernateStats" ]
 }
