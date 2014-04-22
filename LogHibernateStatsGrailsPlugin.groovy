68ca9d56bace81506f9b53490e259a31e01393bc
Igor Shults <ishults@users.noreply.github.com>
2014-04-22 05:34:56 -0500

    Update LogHibernateStatsGrailsPlugin.groovy
    Updating URLs

--- LogHibernateStatsGrailsPlugin.groovy
+++ LogHibernateStatsGrailsPlugin.groovy
@@ -3,9 +3,9 @@ class LogHibernateStatsGrailsPlugin {
     def grailsVersion = "2.0 > *"
     def title = "Log Hibernate Stats Plugin"
     def description = 'Logs Hibernate statistics across controller actions'
-    def documentation = "https://github.com/ishults/logHibernateStats"
+    def documentation = "https://github.com/ishults/log-hibernate-stats"
     def license = "APACHE"
     def developers = [ [ name: "Igor Shults", email: "igor.shults@gmail.com" ]]
-    def issueManagement = [ system: "GitHub", url: "https://github.com/ishults/logHibernateStats/issues" ]
-    def scm = [ url: "https://github.com/ishults/logHibernateStats" ]
+    def issueManagement = [ system: "GitHub", url: "https://github.com/ishults/log-hibernate-stats/issues" ]
+    def scm = [ url: "https://github.com/ishults/log-hibernate-stats" ]
 }
