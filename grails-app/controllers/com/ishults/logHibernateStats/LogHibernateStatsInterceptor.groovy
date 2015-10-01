fcf994f6ffa88dde65e330cf601f492ac23f71c8
Igor Shults <igorshults+github@gmail.com>
2015-10-01 15:15:54 -0500

    Initial Grails 3.0 upgrade changes

---
+++ grails-app/controllers/com/ishults/logHibernateStats/LogHibernateStatsInterceptor.groovy
@@ -0,0 +1,72 @@
+package com.ishults.logHibernateStats
+
+import grails.core.GrailsApplication
+import org.hibernate.stat.Statistics
+
+class LogHibernateStatsInterceptor {
+
+    GrailsApplication grailsApplication
+
+    private static final String START_TIME = 'Hibernate_Start_Time'
+    private static final String ALWAYS = 'ALWAYS'
+    private static final String ALLOWED = 'ALLOWED'
+    private static final String NEVER = 'NEVER'
+
+    def sessionFactory
+
+    LogHibernateStatsInterceptor() {
+        matchAll()
+    }
+
+    boolean before() {
+        String enabledString = grailsApplication.config.getProperty('logHibernateStats.enabled')
+        Statistics stats = sessionFactory.statistics
+println enabledString
+params.test = 3
+        if (enabledString == ALWAYS || (enabledString == ALLOWED && params._logHibernateStats)) {
+            log.info "\n### Start logging for action: ${controllerName}/${actionName} ###"
+
+            if (!stats.statisticsEnabled) {
+                stats.statisticsEnabled = true
+            }
+
+            request[START_TIME] = System.currentTimeMillis()
+
+        } else if (enabledString == ALLOWED && stats.statisticsEnabled) {
+            stats.statisticsEnabled = false // We assume no one else is using stats
+        }
+
+        return true
+    }
+
+    void afterView() {
+        String enabledString = grailsApplication.config.logHibernateStats?.enabled
+
+        if (enabledString == ALWAYS || (enabledString == ALLOWED && params._logHibernateStats)) {
+            Statistics stats = sessionFactory.statistics
+
+            if (!stats.statisticsEnabled) {
+                log.error "Hibernate statistics were disabled during the action: /${controllerName}/${actionName} and could not be logged."
+
+                return
+            }
+
+            Long end = System.currentTimeMillis()
+            Long start = request[START_TIME]
+
+            log.debug """
+############## Hibernate Stats ##############
+Action:                     /${controllerName}/${actionName}
+
+Transaction Count:          ${stats.transactionCount}
+Flush Count:                ${stats.flushCount}
+Prepared Statement Count:   ${stats.prepareStatementCount}
+
+Total time:                 ${end - start} ms
+#############################################
+"""
+
+            stats.clear() // We assume no one else is using stats
+        }
+    }
+}
--- src/main/groovy/com/ishults/logHibernateStats/LogHibernateStatsGrailsPlugin.groovy
+++ src/main/groovy/com/ishults/logHibernateStats/LogHibernateStatsGrailsPlugin.groovy
@@ -1,10 +1,11 @@
+package com.ishults.logHibernateStats
+
 class LogHibernateStatsGrailsPlugin {
-    def version = "0.9"
-    def grailsVersion = "2.0 > *"
+    def grailsVersion = "3.0.0 > *"
     def title = "Log Hibernate Stats Plugin"
     def description = 'Logs Hibernate statistics across controller actions'
     def documentation = "https://github.com/ishults/log-hibernate-stats"
-    def license = "APACHE"
+    def license = "Apache 2.0"
     def developers = [ [ name: "Igor Shults", email: "igor.shults@gmail.com" ]]
     def issueManagement = [ system: "GitHub", url: "https://github.com/ishults/log-hibernate-stats/issues" ]
     def scm = [ url: "https://github.com/ishults/log-hibernate-stats" ]
